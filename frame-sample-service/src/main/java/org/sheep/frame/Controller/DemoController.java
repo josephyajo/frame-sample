3a77ee708dab634702c31dbdb4162b2945fa758d
zhangxuefeng <zhangxuefeng@ccmtjf.com>
2017-07-24 14:04:35 +0800

    init

---
+++ frame-sample-service/src/main/java/org/sheep/frame/Controller/DemoController.java
@@ -0,0 +1,23 @@
+package org.sheep.frame.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Joseph on 2017/6/29.
+ */
+@RestController
+public class DemoController {
+
+    @RequestMapping(value = "helloword1", method = RequestMethod.GET)
+    public String helloword1() {
+        return "abc";
+    }
+
+    @GetMapping("helloword2")
+    public String helloword2() {
+        return "cba";
+    }
+}
---
+++ frame-sample-service/src/main/java/org/sheep/frame/ServiceApplication.java
@@ -0,0 +1,14 @@
+package org.sheep.frame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Joseph on 2017/6/29.
+ */
+@SpringBootApplication
+public class ServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class);
+    }
+}
